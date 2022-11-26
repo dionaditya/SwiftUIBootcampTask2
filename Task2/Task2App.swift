767e49b74b7b8d6cac32c69efdb9e0cc64c71335
Dion Aditya <dion.aditya@kiteworks.com>
2022-11-26 16:37:25 +0700

    Initial Commit

---
+++ Task2/Task2App.swift
@@ -0,0 +1,17 @@
+//
+//  Task2App.swift
+//  Task2
+//
+//  Created by Dion Aditya on 26/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Task2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
