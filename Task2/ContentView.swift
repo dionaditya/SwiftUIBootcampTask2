c2f7d01a5bdae743640f5bd539c9eca7f068c493
Dion Aditya <dion.aditya@kiteworks.com>
2022-11-27 14:56:16 +0700

    feat: task 2 swift ui bottcamp

--- Task2/ContentView.swift
+++ Task2/ContentView.swift
@@ -8,17 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPS
+    
+    @State private var isShown = false
+    @State private var isShowingDetailSheet: Bool = false
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("go-to-beach")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .overlay(
+                    Color.black
+                        .opacity(0.5)
+                        .ignoresSafeArea()
+            )
+            
+            VStack {
+                HStack(spacing: 100) {
+    
+                    Button{
+                        isShown = true
+                    } label: {
+                        Image(systemName: "cloud.sun.fill")
+                            .symbolRenderingMode(.multicolor)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                    } .alert("This feature is not available", isPresented: $isShown) {
+                        Button("OK", role: .cancel) { }
+                    }
+                    
+                    Text("Travel")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                    
+                    Button {
+                        isShowingDetailSheet.toggle()
+                    } label: {
+                        Image(systemName: "cart.badge.plus")
+                            .symbolRenderingMode(.multicolor)
+                            .font(.title2)
+                            .foregroundColor(.white)
+                    }.sheet(isPresented: $isShowingDetailSheet) {
+                        VStack {
+                            SheetDetailView()
+                                .presentationDetents([.fraction(0.45), .fraction(0.6)])
+                                .presentationDragIndicator(.visible)
+                                                            
+                        }
+                    }
+                  
+                }.padding()
+        
+                VStack(alignment: .leading) {
+                    Text("Start your Adventure and Go Travel Now")
+                        .font(.system(size: 45))
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 100))
+                    
+                    Text("Discover local destinations beauty with Bluesky and travel with pride")
+                        .foregroundColor(.white)
+                        .padding(EdgeInsets(top: 5, leading: 30, bottom: 0, trailing: 30))
+                        .fontWeight(.semibold)
+                    
+                }.padding(40)
+                
+                Spacer()
+            
+                
+            } //: VStack
+            .padding()
         }
-        .padding()
+       
     }
 }
 
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Task2/SheetDetailView.swift
@@ -0,0 +1,129 @@
+//
+//  SheetDetailView.swift
+//  Task2
+//
+//  Created by Dion Aditya on 27/11/22.
+//
+
+import SwiftUI
+
+struct CategoryItem: Identifiable {
+    let id = UUID()
+      
+    let title: String, icon: String, color: Color;
+}
+
+struct SheetDetailView: View {
+    // MARK: - Properties
+    var categoryItems: [CategoryItem] = [
+        CategoryItem(title: "Walk", icon: "figure.walk.motion", color: .green),
+        CategoryItem(title: "Food", icon: "fork.knife.circle.fill", color: .orange),
+        CategoryItem(title: "Stay", icon: "house.fill", color: .blue),
+        CategoryItem(title: "Ride", icon: "sailboat.fill", color: .indigo),
+    ]
+    
+    // MARK: - Body
+    var body: some View {
+        VStack {
+            HStack(spacing: 100) {
+                Text("Pink Beach, Flores")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                HStack {
+                    Image(systemName: "star.fill")
+                        .symbolRenderingMode(.multicolor)
+                    Text("4.8")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                } //: HSTACK
+            }
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            ForEach(categoryItems, id: \.id) { category in
+                                CategoryItemView(title: category.title, icon: category.icon, color: category.color)
+                                
+                            }
+                        } //: HSTACK
+            }.padding() //: ScrollView
+            
+            
+            VStack {
+                Text("About Destination")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0))
+                    .frame(
+                        maxWidth: .infinity,
+                        alignment: .leading
+                    )
+                    
+                Text("Pink Beach, or Pantai Merah, as it aptly named, is one of seven pink beaches on the planet, This was awesome")
+                
+                
+            }.frame(
+                maxWidth: .infinity,
+                alignment: .leading
+            ) //: VStack
+             
+            
+            HStack(spacing: 100) {
+                Text("$81,39")
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .fontWeight(.bold)
+                    .padding(EdgeInsets(top: 0, leading: -20, bottom: 10, trailing: 0))
+                
+                Link(destination: URL(string: "https://www.apple.com")!) {
+                          HStack {
+                              Image(systemName: "case")
+                              Text("Join Tour")
+                          }
+                          .frame(width: 140, height: 60)
+                          .foregroundColor(.white)
+                          .background(LinearGradient(colors: [.purple, .indigo], startPoint: .leading, endPoint: .trailing))
+                          .clipShape(Capsule())
+            
+                }
+                      
+            } //: HSTACK
+                .padding()
+            
+            Spacer()
+        } //: VSTACK
+            .padding()
+   
+    }
+}
+
+struct SheetDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetDetailView()
+    }
+}
+
+struct CategoryItemView: View {
+    var title: String
+    var icon: String
+    var color: Color
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            VStack(alignment: .center, spacing: 8) {
+                Image(systemName: icon)
+                    .symbolRenderingMode(.multicolor)
+                    .font(.system(size: 24))
+                    .foregroundColor(color)
+                
+                Text(title)
+                    .foregroundColor(.primary)
+            }
+            
+        } //: HSTACK
+        .frame(width: 50, height: 75)
+        .padding()
+        .background(Color("BrokenWhite"))
+        .cornerRadius(10)
+    }
+}
